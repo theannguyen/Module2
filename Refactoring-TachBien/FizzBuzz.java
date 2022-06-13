115b182df365f1797e3d3206ee8939e269f1161a
theannguyen <104912107+theannguyen@users.noreply.github.com>
2022-06-13 23:38:49 +0700

    thuchanh

---
+++ Refactoring-TachBien/FizzBuzz.java
@@ -0,0 +1,35 @@
+package ThucHanh8;
+
+public class FizzBuzz {
+    public static String fizzBuzz(int number) {
+
+//        //Trước khi tách biến
+//        if(number % 3 == 0 && number % 5 == 0)
+//            return "FizzBuzz";
+//
+//        if(number % 3 == 0)
+//            return "Fizz";
+//
+//        if(number % 5 == 0)
+//            return "Buzz";
+//
+//        return number + "";
+//    }
+
+        //Sau khi tách biến
+        boolean isFizz = number % 3 == 0;
+        boolean isBuzz = number % 5 == 0;
+
+        if (isFizz && isBuzz)
+            return "FizzBuzz";
+
+        if (isFizz)
+            return "Fizz";
+
+        if (isBuzz)
+            return "Buzz";
+
+        return number + "";
+
+    }
+}
