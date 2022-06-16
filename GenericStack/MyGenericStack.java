1e0c58d97a7ae3cc33c24d3831ad67265be9d650
theannguyen <104912107+theannguyen@users.noreply.github.com>
2022-06-17 00:03:53 +0700

    thuchanh11

---
+++ GenericStack/MyGenericStack.java
@@ -0,0 +1,40 @@
+package ThucHanh11;
+
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+//Tạo lớp
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack() {
+        stack = new LinkedList<>();
+    }
+
+    //Cài đặt phương thức push()
+    public void push(T element) {
+        stack.addFirst(element);
+    }
+
+    //Cài đặt phương thức pop()
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+
+    //Cài đặt phương thức size()
+    public int size() {
+        return stack.size();
+    }
+
+    //Cài đặt phương thức isEmpty()
+    public boolean isEmpty(){
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+}
+
+
